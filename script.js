62a203ccfd22328d223de1ebcea8bd253c2d1027
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2021-12-05 15:14:35 +0100

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+const boxesContainer = document.querySelector('.boxes')
+
+createTable()
+
+function createTable() {
+    let text = ""
+    
+    for (let i = 0; i < 9; i++) {
+        text += `
+        <div class="box"></div>
+        `
+    }
+    boxesContainer.innerHTML = text
+}
+
+let boxes = document.querySelectorAll('.box')
+
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', addSymbol)
+}
+
+let symbol = "X" 
+function addSymbol() {
+    if(symbol === "X"){
+        symbol = "O"
+    }else{
+        symbol="X"
+    }
+    this.innerHTML = symbol
+    this.removeEventListener('click', addSymbol)
+  
+}
